a03ea967b080f0391f1c046166a71bdb2afa1b57
reddirani8496 <reddiranibattuluri@gmail.com>
2024-02-02 23:01:58 +0530

    Challenge1 completed

---
+++ Challenge1/FileReader.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+function readFileContent(filePath) {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err.message}`);
+    } else {
+      console.log("File Content:");
+      console.log(data);
+    }
+  });
+}
+
+readFileContent("test-files/file1.txt");
+readFileContent("test-files/empty-file.txt");
+readFileContent("test-files/nonexistent-file.txt");
