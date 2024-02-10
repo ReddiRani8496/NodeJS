fe801d6708075dd070a723ea292a3e4360f3e98e
reddirani8496 <reddiranibattuluri@gmail.com>
2024-02-10 07:14:35 +0530

    Day-3 programs done

--- Day-3/ExpressEx1.js
+++ Day-3/ExpressEx1.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 // by using the app we will access methods get,post,put,delete
 app.get("/", (req, res) => {
@@ -10,8 +11,45 @@ app.get("/about", (req, res) => {
   res.send("update data");
 });
 
+const courses = [
+  { id: 1, name: "javascript" },
+  { id: 2, name: "java" },
+  { id: 3, name: "python" },
+];
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.post("/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/courses/:name", (req, res) => {
+  let course = courses.find((course) => course.name === req.params.name);
+  if (!course) res.status(404).send("course doesn't exist");
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/courses/:name", (req, res) => {
+  let updatedCourses = courses.filter(
+    (course) => course.name !== req.params.name
+  );
+  courses = updatedCourses;
+  res.send(courses);
+});
+
 app.get("/courses/:id", (req, res) => {
-  res.send(req.params.id);
+  // res.send(req.params.id);
+  let course = courses.find((course) => course.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("course doesn't exist");
+  res.send(course);
 });
 
 const port = process.env.port || 3000;
