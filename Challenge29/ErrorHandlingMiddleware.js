984b05d38f02c54258555f0685430730d70ffac6
reddirani8496 <reddiranibattuluri@gmail.com>
2024-02-29 22:24:19 +0530

    Challenge29 done

---
+++ Challenge29/ErrorHandlingMiddleware.js
@@ -0,0 +1,15 @@
+function errorHandler(err, req, res, next) {
+  console.error(err.stack);
+
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message || "Internal Server Error",
+    },
+  });
+}
+
+module.exports = errorHandler;
